bba84255a451e883b91029ef384ca4f9c8a12914
Eleni-Araya <elenib.araya@gmail.com>
2024-04-22 15:49:12 -0500

    final project completed

--- Final_project/music-client/music/src/components/musicPlayer.tsx
+++ Final_project/music-client/music/src/components/musicPlayer.tsx
@@ -2,16 +2,25 @@ import React from 'react'
 
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
+import Songs from '../types/song';
 
 interface MusicPlayerProps {
-    src: string;
+    song: Songs,
+    onNext: () => void,
+    onPrevious: () => void
 }
-export default function MusicPlayer({ src }: MusicPlayerProps) {
+export default function MusicPlayer({ song, onNext, onPrevious }: MusicPlayerProps) {
     return (
         <div>
+            <p>{song.title}</p>
             <AudioPlayer
-                // autoPlay
-                src={src}
+                autoPlay
+                showSkipControls
+                onEnded={onNext}
+                onClickNext={onNext}
+                onClickPrevious={onPrevious}
+                src={'http://localhost:8000/' + song.urlPath}
+
                 onPlay={(e: any) => console.log('onPlay')}
             // other props as needed
             />
--- Final_project/music-client/music/src/components/playList.tsx
+++ Final_project/music-client/music/src/components/playList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useState } from 'react'
 import Songs from '../types/song'
 
 import 'react-h5-audio-player/lib/styles.css';
@@ -11,14 +11,25 @@ import MusicPlayer from './musicPlayer'
 type Props = {
   playlist: Songs[],
   onRemove: (song: string) => void,
-
 }
 export default function PlayListComp(props: Props) {
   const [isPlay, setPlay] = useState<number | null>(null)
+
   const handleClick = (index: number) => {
     setPlay(index)
   }
 
+  const handleNext = () => {
+
+    setPlay(prevIndex => (prevIndex! + 1) % props.playlist.length)
+
+  }
+  const handlePrevious = () => {
+    setPlay(prevIndex => prevIndex === 0 ? props.playlist.length - 1 : prevIndex! - 1)
+
+  }
+
+  // const currentSong = props.playlist[isPlay || 0]
   return (
     <div>
       <h4>Your play List...</h4>
@@ -46,7 +57,9 @@ export default function PlayListComp(props: Props) {
       </table>
       <div className='fixed-bottom'>
         {isPlay !== null && (
-          <div>  {props.playlist.length > 0 ? <MusicPlayer src={'http://localhost:8000/' + props.playlist[isPlay].urlPath} /> : <></>}</div>
+          <div>  {props.playlist.length > 0 ?
+            // <MusicPlayer src={'http://localhost:8000/' + props.playlist[isPlay].urlPath} /> : <></>}</div>
+            <MusicPlayer song={props.playlist[isPlay]} onNext={handleNext} onPrevious={handlePrevious} /> : <></>}</div>
         )}
       </div>
     </div>
